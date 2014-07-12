c6c57bd35ee45fb28fe29f10e478efbf19a60691
William Haynes <william@sabaitechnology.com>
2014-07-12 07:21:32 -0400

    Add changes for PHP

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,6 +3,13 @@
 opkg update
 opkg install php5 php5-cgi php5-fastcgi php5-mod-json php5-mod-session php5-mod-zip libsqlite3 zoneinfo-core php5-mod-pdo php5-mod-pdo-sqlite php5-mod-ctype php5-mod-mbstring php5-mod-gd sqlite3-cli php5-mod-sqlite3 php5-mod-curl curl php5-mod-xml php5-mod-simplexml php5-mod-hash php5-mod-dom php5-mod-iconv
 opkg install php5-mod-mcrypt php5-mod-openssl php5-mod-fileinfo php5-mod-exif
+#make php work
+uci add_list uhttpd.main.interpreter=".php=/usr/bin/php-cgi"
+uci set uhttpd.main.index_page="index.html index.htm default.html default.htm index.php"
+uci commit uhttpd
+sed -i 's,doc_root.*,doc_root = "",g' /etc/php.ini
+sed -i 's,;short_open_tag = Off,short_open_tag = On,g' /etc/php.ini
+/etc/init.d/uhttpd restart
 #install openvpn
 opkg install openvpn pptp
 #php.ini needs to be updated as well as /etc/config/uhttpd
