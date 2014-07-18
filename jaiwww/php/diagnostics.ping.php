2a4fef4a39019f1f6a067843cfda32129060a6c7
William <william@sabaitechnology.com>
2014-07-18 15:32:13 -0400

    fixed to seq in ping listing

--- jaiwww/php/diagnostics.ping.php
+++ jaiwww/php/diagnostics.ping.php
@@ -14,7 +14,7 @@ exec("ping -c $pingCount -s $pingSize " . $pingAddress, $output, $result);
 //$output=json_decode('["PING 192.168.22.11 (192.168.22.11) 56(84) bytes of data.","64 bytes from 192.168.22.11: icmp_req=1 ttl=64 time=0.036 ms","64 bytes from 192.168.22.11: icmp_req=2 ttl=64 time=0.031 ms","64 bytes from 192.168.22.11: icmp_req=3 ttl=64 time=0.032 ms","","--- 192.168.22.11 ping statistics ---","3 packets transmitted, 3 received, 0% packet loss, time 1998ms","rtt min\/avg\/max\/mdev = 0.031\/0.033\/0.036\/0.002 ms"]');
 
 
-$datalines = preg_filter("/^([0-9]*).*: icmp_req=([0-9]*) ttl=([0-9]*) time=([0-9]*.[0-9]* ms)/","$1,$2,$3,$4", $output);
+$datalines = preg_filter("/^([0-9]*).*: seq=([0-9]*) ttl=([0-9]*) time=([0-9]*.[0-9]* ms)/","$1,$2,$3,$4", $output);
 
 $dataResult = array();
 
