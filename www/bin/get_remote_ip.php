bc08dad01eb003a3b43af2b95dc033cbae3e44db
William Haynes <william@sabaitechnology.com>
2014-07-31 07:25:23 -0400

    Moved sys file to /etc/sabai folder

--- www/bin/get_remote_ip.php
+++ www/bin/get_remote_ip.php
@@ -1,9 +1,9 @@
 <?php
 // Sabai Technology - Apache v2 licence
 // copyright 2014 Sabai Technology, LLC
- $URIfile='/www/sys/updateURI';
+ $URIfile='/etc/sabai/sys/updateURI';
  $URI=file_exists($URIfile)?file_get_contents($URIfile):'http://blog.sabaitechnology.com/sabai';
  $lastip=file_get_contents($URI ."/donde.php?plz=kthx");
- file_put_contents("/www/stat/ip",$lastip);
+ file_put_contents("/etc/sabai/stat/ip",$lastip);
  echo $lastip;
 ?>
