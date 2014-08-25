9a2774e88f9b75b68ef6ca1da4a3b2430be82b97
William <william@sabaitechnology.com>
2014-08-25 17:06:04 -0400

    got the table working properly.

--- www/v/security.portforwarding.php
+++ www/v/security.portforwarding.php
@@ -1,63 +1,73 @@
 <form id="fe">
-<input type='hidden' id='pftable' name='pftable'>
-<div class='pageTitle'>Security: Port Forwarding</div>
+  <input type='hidden' id='pftable' name='pftable'>
+  
+  <div class='pageTitle'>Security: Port Forwarding</div>
 
-<div class='controlBox'>
-  <span class='controlBoxTitle'>Port Forwarding</span>
-  <div class='controlBoxContent'> 
-    
-    <table id='list' class='listTable clickable'></table>
-    
-    <input type='button' value='Add' id='add'>
-    <input type='button' id="savebutton" name="savebutton" value='Save' onclick="PORTcall()">
-    <input type='button' value='Cancel' onclick='cancelGateway();'><span id='messages'>&nbsp;</span>
+  <div class='controlBox'>
+    <span class='controlBoxTitle'>Port Forwarding</span>
+    <div class='controlBoxContent'> 
+
+      <table id='list' class='listTable clickable'></table>
+
+      <input type='button' value='Add' id='add'>
+      <input type='button' id="savebutton" name="savebutton" value='Save' onclick="PORTcall()">
+      <input type='button' value='Cancel' onclick='cancelGateway();'>
+      <input type='button' id="deletebutton" name="deletebutton" value='Delete Row' onclick="DELcall()">
+      <span id='messages'>&nbsp;</span>
 
 
-    <div id='hideme'>
+      <div id='hideme'>
         <div class='centercolumncontainer'>
-            <div class='middlecontainer'>
-                <div id='hiddentext' value-'Please wait...' ></div>
-                <br>
-            </div>
+          <div class='middlecontainer'>
+            <div id='hiddentext' value-'Please wait...' ></div>
+            <br>
+          </div>
         </div>
       </div>
 
-    <div class="smallText">
-      <br><b>Proto</b>- Which protocol (tcp or udp) to forward. </li>
-      <br><b>VPN</b> - Forward ports through the normal internet connection (WAN) or through the tunnel (VPN), or both. Note that the Gateways feature may result in may result in undefined behavior when devices routed through an interface have ports forwarded through a different interface. Additionally, ports will only be forwarded through the VPN when the VPN service is active. </li>
-      <br><b>Src Address</b>(optional) - Forward only if from this address. Ex: "1.2.3.4", "1.2.3.4 - 2.3.4.5", "1.2.3.0/24", "me.example.com". </li>
-      <br><b>Ext Ports</b> - The port(s) to be forwarded, as seen from the WAN. Ex: "2345", "200,300", "200-300,400". </li>
-      <br><b>Int Port</b>- The destination port inside the LAN. Only one port per entry is supported. </li>
-      <br><b>Int Address</b>- The destination address inside the LAN. </li>
+      <div class="smallText">
+        <br><b>Proto</b>- Which protocol (tcp or udp) to forward. </li>
+        <br><b>VPN</b> - Forward ports through the normal internet connection (WAN) or through the tunnel (VPN), or both. Note that the Gateways feature may result in may result in undefined behavior when devices routed through an interface have ports forwarded through a different interface. Additionally, ports will only be forwarded through the VPN when the VPN service is active. </li>
+        <br><b>Src Address</b>(optional) - Forward only if from this address. Ex: "1.2.3.4", "1.2.3.4 - 2.3.4.5", "1.2.3.0/24", "me.example.com". </li>
+        <br><b>Ext Ports</b> - The port(s) to be forwarded, as seen from the WAN. Ex: "2345", "200,300", "200-300,400". </li>
+        <br><b>Int Port</b>- The destination port inside the LAN. Only one port per entry is supported. </li>
+        <br><b>Int Address</b>- The destination address inside the LAN. </li>
+      </div>
     </div>
-</div>
-</div>
-<p>
-        <div id='footer'>Copyright © 2014 Sabai Technology, LLC</div>
-</p>
+  </div>
+  <p>
+    <div id='footer'>Copyright © 2014 Sabai Technology, LLC</div>
+  </p>
 </form>
 
 <script type='text/ecmascript'>
-var hidden, hide,res;
-var f = E('fe'); 
-var hidden = E('hideme'); 
-var hide = E('hiddentext');
-var portforwarding;
+  var hidden, hide,res;
+  var f = E('fe'); 
+  var hidden = E('hideme'); 
+  var hide = E('hiddentext');
+  var portforwarding='<?php
+  $pftable=exec("uci get sabai.pf.table");
+  exec("uci get sabai.pf.table > /www/libs/data/port_forwarding.json");
+  echo "$pftable"; 
+  ?>'; 
 
-function PORTcall(){ 
-  hideUi("Adjusting Port Forwarding settings..."); 
+  function PORTcall(){ 
+    $('input[type=search]').val("");
+    $('#example').dataTable().api().search("").draw();
+     hideUi("Adjusting Port Forwarding settings..."); 
 //read the text values
 var TableData=new Array();
 $('#list tr').each(function(row, tr){
-    TableData[row] = {
-        "status" : $(tr).find('td:eq(0)').text()
-        , "proto" : $(tr).find('td:eq(1)').text()
-        , "vpn" : $(tr).find('td:eq(2)').text()
-        , "ext" : $(tr).find('td:eq(3)').text()
-        , "int" : $(tr).find('td:eq(4)').text()
-        , "intaddr" : $(tr).find('td:eq(5)').text()
-        , "description" : $(tr).find('td:eq(6)').text()
-      }
+  TableData[row] = {
+    "status" : $(tr).find('td:eq(1)').text()
+    , "protocol" : $(tr).find('td:eq(2)').text()
+    , "gateway" : $(tr).find('td:eq(3)').text()
+    , "src" : $(tr).find('td:eq(4)').text()
+    , "ext" : $(tr).find('td:eq(5)').text()
+    , "int" : $(tr).find('td:eq(6)').text()
+    , "address" : $(tr).find('td:eq(7)').text()
+    , "description" : $(tr).find('td:eq(8)').text()
+  }
 });
 
 TableData = $.toJSON(TableData);
@@ -67,104 +77,125 @@ TableData = $.toJSON(TableData);
 var json=$.parseJSON(TableData);
 $("#pftable").val(TableData);
 // Pass the form values to the php file 
-  $.post('php/portforwarding.php', $("#fe").serialize(), function(pass){
-    res=$.parseJSON(pass);
+$.post('php/portforwarding.php', $("#fe").serialize(), function(pass){
+  res=$.parseJSON(pass);
 // Detect if values have been passed back   
-    if( res.rMessage != ""){
-      PORTresp();
-    };
-      showUi();
+if( res.rMessage != ""){
+  PORTresp();
+};
+showUi();
 });
 // Important stops the page refreshing
-  return false;
+return false;
 } 
 
 
 function PORTresp(){ 
   msg(res.rMessage); 
   showUi(); 
-  } 
+} 
 
-  var lt =  $('#list').dataTable({
-    'bPaginate': false,
-    'bInfo': false,
-    'stateSave': true,
-    'bProcessing': true,
-    'sAjaxSource': 'libs/ptrs/port_forwarding.json',
-    'aoColumns': [
-      { 'sTitle': 'On/Off',       'mData':'status',     'sClass':'statusDrop'},  
-      { 'sTitle': 'Proto',        'mData':'protocol',   'sClass':'protoDrop' },
-      { 'sTitle': 'VPN',          'mData':'gateway',    'sClass':'vpnDrop' },
-      { 'sTitle': 'Src Address',  'mData':'src',        'sClass':'plainText'  },
-      { 'sTitle': 'Ext Port',     'mData':'ext',        'sClass':'plainText'   }, 
-      { 'sTitle': 'Int Port',     'mData':'int',        'sClass':'plainText' },
-      { 'sTitle': 'Int Address',  'mData':'address',    'sClass':'plainText'  },
-      { 'sTitle': 'Description',  'mData':'description','sClass':'plainText'  }
-    ],
+function DELcall(){ 
+  var datatable = $('#list').DataTable();
+  datatable
+    .rows(':has(:checkbox:checked)')
+    .remove()
+    .draw();
+//$('#rowclick2 tr').filter(':has(:checkbox:checked)').find('td');
+} 
 
-    'fnRowCallback': function(nRow, aData, iDisplayIndex, iDisplayIndexFull){
-      $(nRow).find('.plainText').editable(
-        function(value, settings){ return value; },
-        {
-          'onblur':'submit',
-          'event': 'click',
-          'placeholder' : 'Click to edit'
-        }
+var lt =  $('#list').dataTable({
+  'bPaginate': false,
+  'bInfo': false,
+  'bStateSave': false,
+  'bProcessing': true,
+  'sAjaxSource': 'libs/data/port_forwarding.json',
+  'aoColumns': [
+  { 'sTitle': 'Select',       'mData': null,      "sDefaultContent": '<input type="checkbox" />' },
+  { 'sTitle': 'On/Off',       'mData':'status',     'sClass':'statusDrop'},  
+  { 'sTitle': 'Proto',        'mData':'protocol',   'sClass':'protoDrop' },
+  { 'sTitle': 'VPN',          'mData':'gateway',    'sClass':'vpnDrop' },
+  { 'sTitle': 'Src Address',  'mData':'src',        'sClass':'plainText'  },
+  { 'sTitle': 'Ext Port',     'mData':'ext',        'sClass':'plainText'   }, 
+  { 'sTitle': 'Int Port',     'mData':'int',        'sClass':'plainText' },
+  { 'sTitle': 'Int Address',  'mData':'address',    'sClass':'plainText'  },
+  { 'sTitle': 'Description',  'mData':'description','sClass':'plainText'  }
+  ],
+
+  'fnRowCallback': function(nRow, aData, iDisplayIndex, iDisplayIndexFull){
+    $(nRow).find('.plainText').editable(
+      function(value, settings){ return value; },
+      {
+        'onblur':'submit',
+        'event': 'click',
+        'placeholder' : 'Click to edit'
+      }
       );
 
-      $(nRow).find('.statusDrop').editable(
-        function(value, settings){ return value; },
-        {
-        'data': " {'on':'On','off':'Off'}",
+    $(nRow).find('.statusDrop').editable(
+      function(value, settings){ return value; },
+      {
+        'data': " {'on':'on','off':'off'}",
         'type':'select',
         'onblur':'submit',
         'event': 'click'
-        }
+      }
       );
 
-      $(nRow).find('.protoDrop').editable(
-        function(value, settings){ return value; },
-        {
+    $(nRow).find('.protoDrop').editable(
+      function(value, settings){ return value; },
+      {
         'data': " {'UDP':'UDP','TCP':'TCP', 'Both':'Both'}",
         'type':'select',
         'onblur':'submit',
         'event': 'click'
-        }
+      }
       );
 
-      $(nRow).find('.vpnDrop').editable(
-        function(value, settings){ return value; },
-        {
+    $(nRow).find('.vpnDrop').editable(
+      function(value, settings){ return value; },
+      {
         'data': " {'LAN':'LAN', 'WAN':'WAN','VPN':'VPN'}",
         'type':'select',
         'onblur':'submit',
         'event': 'click'
-        }
+      }
       );
 
-    } /* end fnRowCallback*/
-  }) /* end datatable*/
+  } /* end fnRowCallback*/
+}) /* end datatable*/
 
 
-  $('#add').click( function (e) {
-    e.preventDefault();
-    lt.fnAddData(
-      { 
-      "status": 'Off', 
-      "protocol": 'Both',
-      "gateway": 'WAN',
-      "src": null,
-      "ext": null,
-      "int": null,
-      "address": null,
-      "description": null 
-      }
-    );
+$('#add').click( function (e) {
+  e.preventDefault();
+  lt.fnAddData(
+  { 
+    "status": 'on', 
+    "protocol": 'Both',
+    "gateway": 'WAN',
+    "src": "24.24.24.24",
+    "ext": "15",
+    "int": "56",
+    "address": "192.168.199.2",
+    "description": "Test Data" 
+  }
+  );
+
+});
+
+function ROWcall(){
+  var TableData=new Array();
+  $('#list tr').each(function(row, tr){
+    if ($(tr).find('td:eq(0)').text() == "checked") {
+      $(nRow).addClass('row_selected');
+      list.row('row_selected').remove().draw( false );
+    };
   });
+};
 
-  function saveGateway(){
-    toServer('Save this.');
-  };
+function saveGateway(){
+  toServer('Save this.');
+};
 
   // function toggleExplain(){
 
