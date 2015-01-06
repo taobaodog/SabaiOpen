4ce682a6cf1b360dd0bd25facc8539efa9403082
Kateryna Pietukhova <kateryna.pietukhova@sirisoftware.com>
2015-01-06 12:46:09 +0200

    Removed useless parts of scripts.

--- www/bin/config_search.sh
+++ www/bin/config_search.sh
@@ -2,12 +2,11 @@
 
 number_conf=0
 count=0
-#clear configList.txt
-cat /dev/null > /www/configList
+#clear configList
+cat /dev/null > /tmp/configList
 
 #mounting partition with config files
 mount -t ext4 /dev/sda6 /mnt
-echo mount sda6 command executed with: $? > /dev/kmsg
 
 #searching for config files on partition
 config_list=`ls -p /mnt | grep -v / | sed 's/\/mnt\///'`
@@ -17,16 +16,12 @@ for i in  $(echo "$config_list"); do
 	number_conf=$((number_conf+1))
 done
 
-#try to do JSON
-#echo -e "{" >> /www/configList
+#JSON format
 for i in $config_list; do
 	count=$((count+1))
-	echo -e "\"conf_$count\": \"$i\"," >> /www/configList
+	echo -e "\"conf_$count\": \"$i\"," >> /tmp/configList
 done
-#echo -e \"Number\": \"$number_conf\" >> /www/configList
-#echo "}" >> /www/configList
-strout=`cat /www/configList | tr '\n' ' '| sed 's/.$//' | sed 's/.$//'`
-#strout= $str | sed 's/.$//'
+strout=`cat /tmp/configList | tr '\n' ' '| sed 's/.$//' | sed 's/.$//'`
 echo $strout
 umount /dev/sda6
 
