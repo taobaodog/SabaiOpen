4a7ae25a019de3be90d20c1d78a2da0d7b1c369f
Kateryna Pietukhova <kateryna.pietukhova@sirisoftware.com>
2015-04-17 18:41:26 +0300

    PPTP and OVPN tested, bugs fixed.

--- www/bin/pptp.sh
+++ www/bin/pptp.sh
@@ -7,56 +7,86 @@ act=$1
 config_act=$2
 
 if [ $config_act = "update" ]; then
-	config_file=sabai-new
+	config_file="sabai-new"
 else
-	config_gile=sabai
+	config_file="sabai"
 fi
 
 _stop(){
-    uci delete network.vpn
-    uci delete firewall.vpn
-    uci set network.vpn.proto=none
-    uci commit
-    uci $UCI_PATH set sabai.vpn.status=none
-    uci $UCI_PATH set sabai.vpn.proto=none
-    uci $UCI_PATH commit sabai
-    if [ $config_act = "update" ]; then
-        echo "network" >> /tmp/.restart_services   
-        echo "firewall" >> /tmp/.restart_services
-    else
-	/etc/init.d/network restart
-	/etc/init.d/firewall restart
-    fi
-    logger "pptp stopped and firewall restarted"
+	uci delete network.vpn
+	uci set network.vpn.proto=none
+	uci commit network
+	uci delete firewall.vpn
+	forward=$(uci show firewall | grep forwarding | grep dest=vpn | cut -d "[" -f2 | cut -d "]" -f1 | tail -n 1)                           
+        if [ "$forward" != "" ]; then                                                                                                          
+                uci delete firewall.@forwarding["$forward"]                                                                                    
+        else                                                                                                                                   
+                echo -e /n                                                                                                                     
+        fi                                                                                                                                     
+#        #Allow LAN traffic to WAN                                                                                                              
+#        uci add firewall forwarding                                                                                                            
+#        uci set firewall.@forwarding[-1].src=lan                                                                                               
+#        uci set firewall.@forwarding[-1].dest=wan                                                                                              
+	uci commit firewall	
+	uci $UCI_PATH set sabai.vpn.status=none
+	uci $UCI_PATH set sabai.vpn.proto=none
+	uci $UCI_PATH commit sabai
+	if [ $config_act = "update" ]; then
+		echo "network" >> /tmp/.restart_services   
+		echo "firewall" >> /tmp/.restart_services
+	else
+		/etc/init.d/firewall restart
+		sleep 5
+		/etc/init.d/network restart
+	fi
+   	logger "pptp is stopped and firewall restarted."
 }
 
 _start(){
-    #ensure that openvpn is stopped
-        /etc/init.d/openvpn stop
-        /etc/init.d/openvpn disable
-    #get the pptp settings
-        user=$(uci get sabai.vpn.username)
-        pass=$(uci get sabai.vpn.password)
-        server=$(uci get sabai.vpn.server)
+	status=$(uci get sabai.vpn.status)
+	if [ $status != "none" ]; then
+	#ensure that openvpn is stopped
+		/etc/init.d/openvpn stop
+		/etc/init.d/openvpn disable
+	#ensure that openvpn settings removed
+		uci delete network.sabai
+		uci delete firewall.ovpn
+		forward=$(uci show firewall | grep forwarding | grep dest=sabai | cut -d "[" -f2 | cut -d "]" -f1 | tail -n 1)
+		if [ "$forward" != ""  ]; then
+			uci delete firewall.@forwarding["$forward"]
+		else
+			echo -e "\n"
+		fi
+	fi
+        #Removing LAN to WAN forwarding                                                               
+	#TODO correct                                                                                                           
+    
+	#get the pptp settings
+        user=$(uci get $config_file.vpn.username)
+        pass=$(uci get $config_file.vpn.password)
+	server=$(uci get $config_file.vpn.server)
     #set the network vpn settings
         uci set network.vpn=interface
         uci set network.vpn.ifname=pptp-vpn
-        uci set network.vpn.proto="pptp"
+        uci set network.vpn.proto=pptp
         uci set network.vpn.username="$user"
         uci set network.vpn.password="$pass"
         uci set network.vpn.server="$server"
         uci set network.vpn.buffering=1
+	uci commit network
     #set the firewall
         uci set firewall.vpn=zone
         uci set firewall.vpn.name=vpn
         uci set firewall.vpn.input=ACCEPT
         uci set firewall.vpn.output=ACCEPT
         uci set firewall.vpn.forward=ACCEPT
+	uci set firewall.vpn.network=vpn
         uci set firewall.vpn.masq=1
+	uci add firewall forwarding 
         uci set firewall.@forwarding[-1].src=lan
         uci set firewall.@forwarding[-1].dest=vpn
     #commit all changed services
-        uci commit   
+        uci commit firewall   
     #set sabai vpn settings
         uci $UCI_PATH set sabai.vpn.proto=pptp
         uci $UCI_PATH set sabai.vpn.status=Starting
@@ -67,34 +97,48 @@ _start(){
 		echo "network" >> /tmp/.restart_services
         	echo "firewall" >> /tmp/.restart_services
 	else                                         
-		/etc/init.d/network restart             
-        	/etc/init.d/firewall restart            
+        	/etc/init.d/firewall restart
+		sleep 5            
+		/etc/init.d/network restart
 	fi  
 	
-    logger "pptp run and firewall restarted"
-    sleep 20 
-    if [ $(ifconfig pptp-vpn | grep not) != "" ]; then
-        uci set sabai.vpn.status=Disconnected
-    else
-        uci set sabai.vpn.status=Connected
+	sleep 20 
+	ifconfig > /tmp/check
+	if [ "$(cat /tmp/check | grep tun0)" != "" ]; then
+        	uci set sabai.vpn.status=Disconnected
+		logger "pptp is disconnected."
+	else
+        	uci set sabai.vpn.status=Connected
+		logger "pptp is connected."
         fi
-    uci $UCI_PATH commit sabai
+	uci $UCI_PATH commit sabai
 }
 
 _clear(){
         uci delete network.vpn
         uci delete firewall.vpn
 	uci set network.vpn.proto=none
-        uci commit
+        uci commit network
+	uci delete firewall.vpn
+	forward=$(uci show firewall | grep forwarding | grep dest=vpn | cut -d "[" -f2 | cut -d "]" -f1 | tail -n 1)
+	if [ "$forward" != "" ]; then                                                                                                          
+                uci delete firewall.@forwarding["$forward"]                                                                                    
+        else                                                                                                                                   
+                echo -e /n                                                                                                                     
+        fi
+        #Allow LAN traffic to WAN                                                                                                              
+#        uci add firewall forwarding                                                                                                            
+#        uci set firewall.@forwarding[-1].src=lan                                                                                               
+#        uci set firewall.@forwarding[-1].dest=wan 
+	uci commit firewall
         uci $UCI_PATH delete sabai.vpn.username          
         uci $UCI_PATH delete sabai.vpn.password          
         uci $UCI_PATH delete sabai.vpn.server
         uci $UCI_PATH set sabai.vpn.status=none
         uci $UCI_PATH set sabai.vpn.proto=none
 	uci $UCI_PATH commit sabai
-        /etc/init.d/network restart
         /etc/init.d/firewall restart
-        logger "pptp cleared and firewall restarted"
+        logger "pptp cleared and firewall restarted."
 }
 
 ls >/dev/null 2>/dev/null 
