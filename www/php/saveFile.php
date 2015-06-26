adbb56111182ec9a4a82dc48c1d9fefb8c9c938c
Kateryna Pietukhova <kateryna.pietukhova@sirisoftware.com>
2015-06-26 18:35:33 +0300

    Ovpn config can be saved together with sabai config

--- www/php/saveFile.php
+++ www/php/saveFile.php
@@ -1,7 +1,15 @@
 <?php
 if(isset($_POST['loadFile'])) {
         $file_name = $_POST['loadFile'];
-        $pathToFile = '/configs/'.$file_name;
+	$File = '/configs/'.$file_name;
+	$name = str_replace("backup_", "", $file_name);
+	if (file_exists("/configs/ovpn_backup/ovpn.filename_$name")) {
+		$config="/configs/ovpn_backup/ovpn.config_$name";
+		$msg="/configs/ovpn_backup/ovpn.msg_$name";
+		$ovpn_filename="/configs/ovpn_backup/ovpn.filename_$name";
+		exec("tar -cvf /configs/$file_name.tar $File $config $msg $ovpn_filename");	
+	}	
+        $pathToFile = "/configs/" . $file_name . ".tar" ;
         echo $pathToFile;
 }
 ?>
