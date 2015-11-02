23da84dd84d8d5fce5e3491abe247e22b0ce1bd8
William Haynes <william@sabaitechnology.com>
2015-11-02 10:28:05 -0500

    Create wanfix.sh
    Ensure that eth0 subnet can route through tunnel.

---
+++ etc/sabai/accelerator/wanfix.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+route="$(ip route | grep eth0 | grep '.0/' | awk '{print $1}')"
+iptables -I FORWARD -s $route -o tun+ -j ACCEPT
+iptables -I FORWARD -s $route -o ppp+ -j ACCEPT
+iptables -t nat -I POSTROUTING -o ppp+  -j MASQUERADE
+iptables -t nat -I POSTROUTING -o tun+  -j MASQUERADE
