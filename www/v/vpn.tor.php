ce394ba21da0a57d883c8837e8bd6017b4d44567
Kateryna Pietukhova <kateryna.pietukhova@sirisoftware.com>
2015-11-04 20:27:11 +0200

    Added tor WEB page.

---
+++ www/v/vpn.tor.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<!--Sabai Technology - Apache v2 licence
+    copyright 2015 Sabai Technology -->
+
+<form id="fe">
+<div class='pageTitle'>VPN: Tor - Anonymity Online</div>
+<div class='controlBox'><span class='controlBoxTitle'>Tor Settings</span>
+    <div class='controlBoxContent'>
+	<body onload='init();' id='topmost'>
+        <input type='hidden' id='act' name='act'>
+        <div class='section'>
+            <table class="fields">
+                <tbody>
+                    <tr>
+                       
+ 					</tr>
+                </tbody>
+            </table>
+        </div>
+        </form>
+    <div id='hideme'>
+        <div class='centercolumncontainer'>
+            <div class='middlecontainer'>
+                <div id='hiddentext'>Please wait...</div>
+                <br>
+            </div>
+        </div>
+    </div>
+    <p>
+        <div id='footer'>Copyright © 2015 Sabai Technology, LLC</div>
+    </p>
+</body>
+
+<script type='text/javascript'>
+   
+</script>
+
