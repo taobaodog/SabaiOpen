5a61ab1d9d43efb23372ced691d8423681af55ec
Kateryna Pietukhova <kateryna.pietukhova@sirisoftware.com>
2015-11-17 17:13:37 +0200

    Fixes for compatibility of acc and router

--- etc/sabai/accelerator/wanfix.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Sabai Technology - Apache v2 licence
-# Copyright 2015 Sabai Technology
-
-route="$(ip route | grep eth0 | grep '.0/' | awk '{print $1}')"
-iptables -I FORWARD -s $route -o tun+ -j ACCEPT
-iptables -I FORWARD -s $route -o ppp+ -j ACCEPT
-iptables -t nat -I POSTROUTING -o ppp+  -j MASQUERADE
-iptables -t nat -I POSTROUTING -o tun+  -j MASQUERADE
