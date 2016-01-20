adb67d62744ae146071bfba15eb21b670d44bb65
Kateryna Pietukhova <kateryna.pietukhova@sirisoftware.com>
2016-01-20 18:07:41 +0200

    CLOSED: #136 OpenVPN file name. #147 File name for upload

--- www/php/download_config.php
+++ www/php/download_config.php
@@ -2,16 +2,19 @@
 // Sabai Technology - Apache v2 licence
 // Copyright 2015 Sabai Technology, LLC
 
-$uploaddir = '/configs/';
+$UCI_PATH = "-c /configs";
+
+$uploaddir = "/configs/";
 $ovpn_dir = $uploaddir . "ovpn_backup/";
 
-
+function upload_config(){
 if (!empty($_FILES['_browse1']['tmp_name'])) {
-        $uploadfile = basename($_FILES['_browse1']['name']);
+	$uploadfile = preg_replace("/[^a-zA-Z0-9.]/", "_", $_FILES['_browse1']['name']);
 	$tmp="/tmp/".$uploadfile;
+	exec("logger $uploadfile");
 	copy($_FILES['_browse1']['tmp_name'], $tmp) or die( "Could not copy file!");
 	$file_type = pathinfo($tmp, PATHINFO_EXTENSION);
-	
+	exec("logger $uploadfile $file_type");	
 	if ($file_type == "tar") {
 		$name = str_replace(".tar", "", $uploadfile);
 		$check = strpos($name, "backup_");
@@ -22,10 +25,9 @@ if (!empty($_FILES['_browse1']['tmp_name'])) {
 		}
 		exec("mkdir /tmp/$name");
 		exec("tar -xvf $tmp -C /tmp/$name");
-		if (copy("/tmp/" . $name . "/configs/" . $name,$uploaddir.$name)) {
+		if (copy("/tmp/" . $name . "/configs/" . $name, "/configs/".$name)) {
 			if (file_exists($uploaddir.$name)) {
-				exec("ln -s $uploaddir$name /etc/config/$name");
-				$get_version = exec("uci get $name.general.version");
+				$get_version = exec("uci -c /configs get $name.general.version");
 				if ($get_version != "") {
 					$ovpn_tmp = "/tmp/" . $name . "/configs/ovpn_backup/";
 					$copy_config = copy($ovpn_tmp . "ovpn.config_" . $file, $ovpn_dir."ovpn.config_".$file);
@@ -37,6 +39,7 @@ if (!empty($_FILES['_browse1']['tmp_name'])) {
 						echo "OVPN configuration was not uploaded.";
 					} 
 				} else {
+					exec("rm /configs/$name");
 					echo "Failed.You tried to upload some wrong configuration.";
 				}
 			} else {
@@ -45,27 +48,33 @@ if (!empty($_FILES['_browse1']['tmp_name'])) {
                 } else {
                         echo "Something went wrong. Could not upload your file.";
                 }
-		exec("rm /etc/config/$file");
 		exec("rm -r /tmp/$name");
 		exec("rm -r $tmp");
         } else {
-		exec("mv $tmp $uploaddir$uploadfile");
-		if (file_exists($uploaddir.$uploadfile)) {
-			exec("ln -s $uploaddir$uploadfile /etc/config/$uploadfile");
-			$get_version = exec("uci get $uploadfile.general.version");
+		if (copy($tmp, "/configs/".$uploadfile)) {
+			$get_version = exec("uci -c /configs get $uploadfile.general.version");
 			if ($get_version != "") {
 				echo "true";
 			} else {
-				exec("rm $uploaddir$uploadfile");
+				exec("rm /configs/$uploadfile");
 				echo "Failed.You tried to upload some wrong configuration.";
 			} 
 		} else {
 			echo "Something went wrong. No file was uploaded.";
 		}
+		exec("rm /tmp/$uploadfile");
 		exec("rm /etc/config/$uploadfile");		
 	}
 } else {
         echo "false";
+}
+}
+
+if (file_exists($ovpn_dir)) {
+	upload_config();
+} else {
+	exec("mkdir $ovpn_dir");
+	upload_config();
 }
 
 ?>
