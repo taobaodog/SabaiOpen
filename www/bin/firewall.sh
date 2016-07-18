ef5d12973df9bd95f643f0df5aaf696b3ff7d610
Kateryna Pietukhova <kateryna.pietukhova@sirisoftware.com>
2016-07-18 15:57:33 +0300

    FIXES

--- www/bin/firewall.sh
+++ www/bin/firewall.sh
@@ -3,226 +3,108 @@
 # Copyright 2016 Sabai Technology
 # iptables rules are stored in /etc/sabai/firewall.settings
 
-#To Do get Enable WAN access working - Test
+val=$1
+state=$2
 
-###################################################
-#this script developed to set four variables to be 
-#on or off according to user input 
-###################################################
+_ping(){
+	local state=$1
 
-UCI_PATH=""
+	local field=$(uci show firewall | grep -e "name='Disable ping from WAN'" | cut -d "[" -f2 | cut -d "]" -f1)
 
-###################################
-#SCRIPT HELP 
-###################################
-
-###############
-#Used Variables 
-###############
-if [ $# -ne 4 ]; then
-	action=$1
-	config_file=sabai-new
-	icmp=$(uci get $config_file.firewall.icmp)
-	multicast=$(uci get $config_file.firewall.multicast)                                                                                                
-        cookies=$(uci get $config_file.firewall.cookies)                                                                                                  
-        wanroute=$(uci get $config_file.firewall.wanroute)	
-else
-	action=$save
-	config_file=sabai 
-	cat << 'EOF'
-   
-this script for setting four parameters
-usage: 
-#./firewall.sh $icmp $multicast $cookies $wanroute 
-Parameters: 
-[icmp]  on/off this to enable/disable ping           
-[multicast] on/off this is to enable/disable udp multicast  
-[cookies] on/off this to enable/disable syn-cookie 
-[wanroute] this is to enable/disable external access to router  
-                                       
-Examples:
-to run use the following 
-#firewall.sh on off on off  
-  
-EOF
-	
-	icmp=$1
-	multicast=$2
-	cookies=$3
-	wanroute=$4
-
-	uci set $UCI_PATH $config_file.firewall.icmp=$icmp
-	uci set $UCI_PATH $config_file.firewall.multicast=$multicast
-	uci set $UCI_PATH $config_file.firewall.cookies=$cookies
-	uci set $UCI_PATH $config_file.firewall.wanroute=$wanroute
-	uci $UCI_PATH commit $config_file
-	cp -r /etc/config/sabai /configs/
-fi
-
-#wanport=$(uci get network.wan.ifname);
-
-#############################
-#Script Function 
-#############################
-
-#########################
-#set wan response to ping
-#########################
-if [ "$icmp" = "on" ] 
-then
-     #turn on icmp response on wan sidea
-     #disabling/enabling icmp ping using /etc/sysctl.conf "net.ipv4.icmp_echo_ignore_all" variable
-     #checking if strng  is exist 
-     cat /etc/sysctl.conf | grep -q "net.ipv4.icmp_echo_ignore_all" 
-     if [ $? -eq 0 ] 
-     then 
-           #string existed just update it to be 0 or 1 inorder to enable/disable icmp ping
-           cat /etc/sysctl.conf | grep  "net.ipv4.icmp_echo_ignore_all" | grep -q "0" 
-           if [ $? -eq 0 ] 
-           then
-		echo "icmp response on"  
-           else
-		echo "enabling icmp response ...." 
-		sed -i 's/net.ipv4.icmp_echo_ignore_all=1/net.ipv4.icmp_echo_ignore_all=0/'  /etc/sysctl.conf 
-           fi 
-     else 
-	#string not found append string to the file 
-	echo "net.ipv4.icmp_echo_ignore_all=0" >> /etc/sysctl.conf 
-     fi  
-      
-elif [ "$icmp" = "off" ] 
-then 
-	echo "you entered icmp off"
-	cat /etc/sysctl.conf | grep -q "net.ipv4.icmp_echo_ignore_all"
-	if [ $? -eq 0 ]
-	then
-		cat /etc/sysctl.conf | grep  "net.ipv4.icmp_echo_ignore_all" | grep -q "1"
-    	if [ $? -eq 0 ]
-		then
-			echo "icmp response off"
-		else
-			echo "disabling icmp response .." 
-			sed -i 's/net.ipv4.icmp_echo_ignore_all=0/net.ipv4.icmp_echo_ignore_all=1/'  /etc/sysctl.conf
-		fi
-	else
-		echo "net.ipv4.icmp_echo_ignore_all=1" >> /etc/sysctl.conf
-	fi                                                                                                      
-else 	
-	echo "ERROR invalid icmp only on/off Accepted"
-fi
-sysctl -newp | grep -q done 
-
-
-###################################
-###set ability to receive multicast
-###################################
-
-#here we are going to enable/disable UDP mulitcasting 
-# should prevent unnecessary traffic on ports that were not actually subscribing.
-#this is done by enable  IGMP snooping 
-
-if [ "$multicast" = "on" ]
-then
-     
-	#turn on multicast 
-	echo "UDP  multicast is enabled "
-	#1-INSTALL IGMPPROXY
-	opkg update
-	opkg install igmpproxy
-
-	#2-enableigmpsnooping in /etc/config/network 
-	uci set network.lan.igmp_snooping=1
-	uci commit network 
-
-	#3-configure firewall to accept igmp 
-	rule=$(uci show firewall | grep =igmp | cut -d "[" -f2 | cut -d "]" -f1 | tail -n 1)
-	if [ "$rule" = "" ]; then                                                                              
-		uci add firewall rule
-		uci set firewall.@rule[-1].src=wan
-		uci set firewall.@rule[-1].proto=igmp
-		uci set firewall.@rule[-1].target=ACCEPT	
-		uci commit firewall
-		uci add firewall rule                                                                                
-                uci set firewall.@rule[-1].src=wan                                                                   
-                uci set firewall.@rule[-1].proto="tcpudp"
-		uci set firewall.@rule[-1].dest=lan
-		uci set firewall.@rule[-1].target=ACCEPT
-		uci set firewall.@rule[-1].family=ipv4 
-		uci commit firewall
-	else 
-		echo -n 
+	if [ -n "$field" ]; then
+		uci delete firewall.@rule[$field]
 	fi
-	
-	#4-configure igmpproxy 
-	cat /etc/igmpproxy.conf | grep -q wan 
-	if [ $? -ne 0 ]
-	then 
-		echo "phyint wan upstream ratelimit 0 threshold 1
-		phyint lan downstream ratelimit 0 threshold 1" >>  /etc/igmpproxy.conf
-	else 
-		echo -n 
+
+	if [ "$state" == "disabled" ]; then
+		uci add firewall rule > /dev/null
+		uci set firewall.@rule[-1].name='Disable ping from WAN'
+		uci set firewall.@rule[-1].src='wan'
+		uci set firewall.@rule[-1].proto='icmp'
+		uci set firewall.@rule[-1].icmp_type='8'
+		uci set firewall.@rule[-1].target='DROP'
 	fi
-elif [ "$multicast" = "off" ]
-then
-	echo "udp multicast  disabled"
-	uci set network.lan.igmp_snooping=0
-	uci commit network
-	rule=$(uci show firewall | grep =igmp | cut -d "[" -f2 | cut -d "]" -f1 | tail -n 1)
-	# removing 2 rules, which was added for enabling
-	uci delete firewall.@rule["$rule"]
-	uci delete firewall.@rule["$rule"]
+
 	uci commit firewall
-else
-	echo "ERROR invalid multicast only on/off Accepted"
-fi
+	logger "Ping from WAN is $state. Restarting firewall"
+	/etc/init.d/firewall restart 2>1 > /dev/null
+}
 
-################                         
-#Set Syn Cookies
-################ 
-if [ "$cookies" = "on" ]
-then
-	#turn on Syn cookie 
-	echo "Syn-Cookies is on"
-	#turn on Syn cookie                                                                          
-        echo "Syn-Cookies is on"                                                                                     
-        #setting tcp syncookies to 1
-	uci set firewall.@defaults[].tcp_syncookies=1
-          
-elif [ "$cookies" = "off" ]
-then
-	echo "Syn-Cookies is off"                                                         
-	#setting tcp syncookies to 0                                                                                 
-        uci set firewall.@defaults[].tcp_syncookies=0  
-else
-	echo "ERROR invalid cookies only on/off Accepted"   
-fi
+_multicast(){
+	local state=$1
 
-uci commit firewall
+	for field in $(uci show firewall | grep -e "name='Enable UDP multicast (IGMP)'" | cut -d "[" -f2 | cut -d "]" -f1 | sort -r)
+	do
+		uci delete firewall.@rule[$field]
+	done
+	uci commit firewall
 
-##################
-#external wan Route
-##################
+	if [ "$state" == "enabled" ]; then
+		uci set network.lan.igmp_snooping=1
+		uci commit network
 
-#allow wan route input
-if [ "$wanroute" = "on" ]
-then
-	#turn on multicast
-	echo "you enabled external wan route "
-elif [ "$wanroute" = "off" ]
-then
-	echo "you disabled external wanroute"
-else
-	echo "ERROR invalid wanroute only on/off Accepted"
-fi
+		uci add firewall rule > /dev/null
+		uci set firewall.@rule[-1].name='Enable UDP multicast (IGMP)'
+		uci set firewall.@rule[-1].src='wan'
+		uci set firewall.@rule[-1].proto='igmp'
+		uci set firewall.@rule[-1].target='ACCEPT'
+		uci commit firewall
 
-if [ $action = "update" ]; then
-	echo "firewall" >> /tmp/.restart_services
-	echo "network" >> /tmp/.restart_services
-else
-	/etc/init.d/firewall restart
-	/etc/init.d/network restart
-	logger "firewall run and restarted"
-	# restart any services like firewall or network that need it.
-fi
+		uci add firewall rule > /dev/null
+		uci set firewall.@rule[-1].name='Enable UDP multicast (IGMP)'
+		uci set firewall.@rule[-1].src='wan'
+		uci set firewall.@rule[-1].proto='udp'
+		uci set firewall.@rule[-1].dest='lan'
+		uci set firewall.@rule[-1].target=ACCEPT
+		uci set firewall.@rule[-1].family=ipv4
+		uci commit firewall
 
-ls >/dev/null 2>/dev/null 
+	else
+		uci set network.lan.igmp_snooping=0
+		uci commit network
+	fi
+
+	logger "UDP multicast (IGMP) is $state. Reloading network and restarting firewall"
+	/etc/init.d/network reload 2>1 > /dev/null
+	/etc/init.d/firewall restart 2>1 > /dev/null
+}
+
+_cookies(){
+	local state=$1
+
+	if [ "$state" == "enabled" ]; then
+		uci set firewall.@defaults[0].tcp_syncookies=1
+	else
+		uci set firewall.@defaults[0].tcp_syncookies=0
+	fi
+	uci commit firewall
+
+	logger "SYN cookies are $state. Restarting firewall"
+	/etc/init.d/firewall restart 2>1 > /dev/null
+}
+
+_wan_access(){
+	local state=$1
+	local ip_lan="$(ifconfig -a br-lan | grep -F "inet addr" | awk '{print $2}' | tr -d 'addr:')"
+
+	if [ "$state" == "enabled" ]; then
+		uci set uhttpd.main.listen_http=80
+		uci set uhttpd.main.listen_https=443
+		uci delete dropbear.@dropbear[0].Interface
+	else
+		uci set uhttpd.main.listen_http=$ip_lan':80'
+		uci set uhttpd.main.listen_https=$ip_lan':443'
+		uci set dropbear.@dropbear[0].Interface='lan'
+	fi
+	uci commit
+
+	logger "WAN access is $state. Restarting uhttpd and dropbear"
+	/etc/init.d/uhttpd restart
+	/etc/init.d/dropbear restart
+}
+
+case $val in
+	ping)		_ping $state	   ;;
+	multicast)   _multicast $state  ;;
+	syn_cookies) _cookies $state	;;
+	wan_access)  _wan_access $state ;;
+esac
--- www/bin/upnp.sh
+++ www/bin/upnp.sh
@@ -1,18 +1,18 @@
 #!/bin/ash
 # Sabai Technology - Apache v2 licence
 # Copyright 2016 Sabai Technology
-#this script developed to set three variables to be 
-#on or off according to user input 
+#this script developed to set three variables to be
+#on or off according to user input
 #As well as specify the start and end of the internal and external ports
 
-#Used Variables 
+#Used Variables
 
 action=$1
 
-if [ $action = "update" ]; then
-        config_file=sabai-new
+if [ "$action" = "update" ]; then
+	config_file=sabai-new
 else
-        config_file=sabai
+	config_file=sabai
 fi
 
 enable=$(uci get $config_file.upnp.enable);
@@ -27,42 +27,43 @@ extmax=$(uci get $config_file.upnp.extmax);
 extrange="$extmin-$extmax"
 
 
-#Script Function 
+#Script Function
 if [ "$enable" = "on" ]; then
-  	uci set upnpd.config.enable_upnp=1
-  	uci set upnpd.@perm_rule[0].int_ports=$intrange
+	uci set upnpd.config.enable_upnp=1
+	uci set upnpd.@perm_rule[0].int_ports=$intrange
 	uci set upnpd.@perm_rule[0].ext_ports=$extrange
 else
-  	uci set upnpd.enable_upnp=0
+	uci set upnpd.enable_upnp=0
 fi
 
 if [ "$natpmp" = "on" ]; then
-  	uci set upnpd.config.enable_natpmp=1
-  else
-  	uci delete upnpd.config.enable_natpmp
+	uci set upnpd.config.enable_natpmp=1
+else
+	uci set upnpd.config.enable_natpmp=0
 fi
- 
+
 if [ "$clean" = "on" ]; then
-  	uci set upnpd.config.clean_ruleset_interval=600
-  else
-  	uci delete upnpd.config.clean_ruleset_interval
+	uci set upnpd.config.clean_ruleset_interval=600
+else
+	uci set upnpd.config.clean_ruleset_interval=0
 fi
 
 
 if [ "$secure" = "on" ]; then
-  	uci set upnpd.config.secure_mode=1
+	uci set upnpd.config.secure_mode=1
 else
-	uci delete upnpd.config.secure_mode
+	uci set upnpd.config.secure_mode=0
 fi
 
 
 uci commit upnpd
 
-if [ $action = "update" ]; then
+if [ "$action" = "update" ]; then
 	echo "firewall" >> /tmp/.restart_services
 else
+	/etc/init.d/miniupnpd restart
 	/etc/init.d/firewall restart
 	logger "upnp script run and firewall restarted"
 fi
 
-ls >/dev/null 2>/dev/null 
+ls >/dev/null 2>/dev/null
