4fcb50d1c153135fd7702ed5b644ac7da6aef755
Kateryna Pietukhova <kateryna.pietukhova@sirisoftware.com>
2016-09-13 17:09:48 +0300

    Backend to all dataTables

--- www/php/saveTable.php
+++ www/php/saveTable.php
@@ -3,5 +3,15 @@
 	$data_table_id = $_POST["table"];
 	$data = json_encode($data_rows, true);
 
-	file_put_contents("/www/libs/data/network.time.json", $data);
+	switch ($data_table_id) {
+		case 'NTPTable':
+			file_put_contents("/www/libs/data/network.time.json", $data);
+			break;
+		case 'gateTable':
+			file_put_contents("/www/libs/data/dhcp.json", $data);
+			break;
+		case 'portTable':
+			file_put_contents("/www/libs/data/port_forwarding.json", $data);
+			break;
+	}
 ?>
