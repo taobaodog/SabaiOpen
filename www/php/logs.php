256ef1e80fdb5d5d0b9b9b78b3c77e8d45f87a60
Kateryna Pietukhova <kateryna.pietukhova@sirisoftware.com>
2016-10-05 15:32:54 +0300

    CLOSED: #340 Backend fixes

--- www/php/logs.php
+++ www/php/logs.php
@@ -1,89 +1,55 @@
 <?php
 // Sabai Technology - Apache v2 licence
 // Copyright 2016 Sabai Technology
- if (!headers_sent()){ header('Content-type: text/ecmascript'); }
- $logPath = '/var/log/';
-
-function listFiles($path, $prefix = '', $sublist = false, $pathName = ''){
-	if(empty($path) || (!is_readable($path)) || (!($files = scandir($path))) || empty($files) ) return;
-	if($sublist){
-		$eid = strtr(trim($path,"/"),"/","-");
-		echo "$prefix<li class='sublist'>\n";
-		echo "$prefix<span class='dir closed' rel='$eid'>$pathName/</span><ul class='directory' id='$eid'>\n";
-	}else{
-		echo "<ul id='listRoot' class='dirlist'>\n";
-	}
-	sort($files, SORT_NATURAL | SORT_FLAG_CASE);
-	foreach($files as $f){
-		if($f=='.' || $f=='..') continue; // ignore . and ..
-		if(is_dir( ($fpath = rtrim($path,'/') .'/' .$f) )){
-			listFiles($fpath, $prefix."\t", true, $f);
-		}else{
-			echo "$prefix\t<li>$f</li>\n";
-		}
-	}
-	echo "$prefix</ul>\n";
-	if($sublist) echo "$prefix</li>\n";
-}
-
-function kernelLog(){
-	if (file_exists("/configs/log/kernel_log")) {
-		exec("dmesg >> /configs/log/kernel_log");
-	} else {
-		exec("mkdir /configs/log/");
-		exec("dmesg > /configs/log/kernel_log");
-	}
-}
 
 $act=array_key_exists('act', $_REQUEST) ? $_REQUEST['act'] : null;
 $log=array_key_exists('log', $_REQUEST) ? $_REQUEST['log'] : null;
 $detail=array_key_exists('detail', $_REQUEST) ? $_REQUEST['detail'] : null;
 
-
-$validPath = realpath($logPath . $log);
-
-
-
-$isZipped = ( pathinfo($validPath, PATHINFO_EXTENSION) == 'gz' );
+$logPath = '/var/log/';
+$validPath = $logPath.$log;
+$isZipped = (pathinfo($validPath, PATHINFO_EXTENSION) == 'gz');
 $detail = escapeshellarg($detail);
 
+#TODO: rework file handling
+if ("$log" == "kernel.log") {
+	if (file_exists($validPath)) {
+		exec("dmesg >> \"". $validPath ."\"");
+	} else {
+		exec("dmesg > \"". $validPath ."\"");
+	}
+}
+
+if (file_exists($validPath) && filesize($validPath)) {
 switch ($act) {
 	case 'all':
 		if ($isZipped){
 			passthru("gunzip -c $validPath");
 		}else{
-			readfile($validPath);	
+			readfile($validPath);
 		}
 	break;
-
 	case 'head':
 	case 'tail':
 		$detail = '-n '. $detail;
 	case 'grep':
 		passthru( $isZipped ? "gunzip -c $validPath | $act $detail" : "$act $detail $validPath" );
 	break;
-	case 'dmesg':
-		kernelLog();
-		readfile("/configs/log/kernel_log");
-	break;
 	case 'download':
-		if (file_exists("/var/log/messages")) {
-			exec("mkdir /configs/log/");
-			exec("cat /var/log/messages >> /configs/log/syslog");
-			kernelLog();
-			exec("rm /configs/logs.tar");
-			exec("tar -cvf /configs/logs.tar /configs/log/syslog /configs/log/kernel_log");
-			$pathToFile = "/configs/logs.tar";
-		} else {
-			$pathToFile = "false";
-		}
-		echo $pathToFile;
+			$pathToFile = "/configs/log/" . $log;
+			if (file_exists("/configs/log/")) {
+				copy($validPath, $pathToFile);
+			} else {
+				mkdir("/configs/log", 0700);
+				copy($validPath, $pathToFile);
+			}
+			echo $pathToFile;
 	break;
-	
-	case 'list':
 	default:
-		listFiles($logPath);
+		echo "No log file was found.";
 	break;
+} } else {
+	echo "false";
 }
 
 ?>
--- www/v/diagnostics.logs.php
+++ www/v/diagnostics.logs.php
@@ -11,63 +11,6 @@ if (basename($_SERVER['PHP_SELF']) == basename(__FILE__)) {
 
 #logContents { width: 100%; height: 40em; }
 
-
-/*#listContainer {
- position: relative;
- display: inline-block;
- border: 1px solid transparent;
- width: 25%;
- margin: .25em .5em 0 0;
-}
-
-#currentLog {
- display: inline-block;
- padding: .1em .5em;
-/* cursor: pointer;
-}
-*/
-/*#currentLog, .dirlist > li, .dir { cursor: pointer; }*/
-
-/*#listRoot {
- width: 100%;
- display: none;
- background: white;
- border: 1px solid black;
- float: left;
- margin: 0 0 0 -1px;
- margin: 0;
- cursor: pointer;
- text-indent: 0;
- padding: .1em 0;
- list-style-type: none;
- z-index: 2;
- position: absolute;
-}*/
-/*.dirlist > li { padding: 0 .5em; }
-.dirlist > li:hover { background-color: yellow; }
-.dirlist > li.sublist:hover { background-color: silver; }
-*/
-/*.dir {
-	display: inline-block;
-	width: 100%;
-}
-
-.directory {
- display: none;
- text-indent: 0;
- margin: 0;
- padding-left: 0;
- list-style-type: none;
-}
-.directory > li {
- padding-left: 1em;
-}
-
-.closed:before { content: "+ "; }
-.open:before { content: "- "; }
-
-#goButton { float: right; }*/
-
 </style>
 
 <div class='pageTitle'>Diagnostics: Logs</div>
@@ -75,27 +18,14 @@ if (basename($_SERVER['PHP_SELF']) == basename(__FILE__)) {
 <div class='controlBox'>
 	<span class='controlBoxTitle'>Logs</span>
 	<div class='controlBoxContent'>
-		<!-- <input id="log" name="log" type="hidden"> -->
-
-<?php// include("php/logs.php"); ?>
-
-<!-- <div id="listContainer">
-
-<span id="currentLog" onclick='showLogSelect();'></span>
-
-<ul id="listRoot" class='dirlist'>
-
-</ul>
-
-</div> -->
 <div class="row">
     <div class="col-md-12 col-sm-12 col-xs-12">
         <form id='fe' class="form-inline" role="form">
             <div class="form-group">
               	<select id='log' class="form-control" name='log'>
 	 				<option value='messages' selected>System log</option>
-	 				<option value='privoxy'>Privoxy log</option>
-	 				<option value='kernel'>Kernel log</option>
+	 				<option value='privoxy.log'>Privoxy log</option>
+	 				<option value='kernel.log'>Kernel log</option>
 	 			</select>
             </div>
             <div class="form-group">
@@ -118,8 +48,6 @@ if (basename($_SERVER['PHP_SELF']) == basename(__FILE__)) {
 		<button class='btn btn-default btn-sm pull-left' id='goButton' type="button" value="Go" onclick="goLog();">Show</button>
 	</div>
 <br>	
-
-
 		<div id='hideme'>
             		<div class='centercolumncontainer'>
                 		<div class='middlecontainer'>
@@ -143,9 +71,7 @@ var hidden = E('hideme');
 var hide = E('hiddentext');
 
 function goLog(n){
-
 console.log($("#fe").serialize());
-
 	if($("#act").val() == "download"){
 		hideUi("Downloading ...");
 		$.ajax("php/logs.php", {
@@ -165,15 +91,19 @@ console.log($("#fe").serialize());
 		});
 	}else{
 		$.ajax("php/logs.php", {
-			success: function(o){ $('#logContents').html(o); },
+			success: function(data){ 
+				if (data.trim() == "false") {
+					hideUi("Log file is empty.");
+					setTimeout(function(){showUi()},4500);
+				} else {
+					$('#logContents').html(data);
+				}
+			},
 			dataType: "text",
 			data: $("#fe").serialize()
 		});
 	}
 }
-
-/*function catchEnter(event){ if(event.keyCode==13) goLog(); }*/
-
 
 function toggleDetail(){
 	$('#detailSuffix').html('');
@@ -203,56 +133,8 @@ function toggleDetail(){
   });
 }); 
 
-/*function toggleContentList(event){
-	$(this).toggleClass("closed open")
-	$('#'+ $(this).attr('rel') ).slideToggle();
-}
-
-function setLogValue(logName){
-	$('#log').val(logName);
-	$('#currentLog').html(logName);
-}
-
-function hideLogSelect(){
-	setLogValue( getLogSelected( this ) );
-	$('#listRoot').slideUp('fast');
-}*/
-
 $(function(){
-/* $('#detail').on("keydown", catchEnter);
- $('.dir').on("click", toggleContentList);
- $('#listRoot li').not('.sublist').on("click", hideLogSelect);
- setLogValue('messages');*/
  toggleDetail();
 });
 
-/*
-function show(){ $('#tooltip').show(); }
-function hide(){ $('#tooltip').hide(); }
-function displayInline(){ if($('#inlineHelp').is(':checked')){ $('.inlineHelp').show(); }else { $('.inlineHelp').hide(); } }
-function checkNum(){
-	console.log('blurred');
-	var contents = $('#lines').val();
-	console.log(contents)
-	if($.isNumeric(contents)){
-		console.log('numberic')
-		$('#error').html('');
-		$('#linesDiv').removeClass('errorInput')
-		getLog('last');
-	}else{
-		console.log('not numberic')
-		$('#error').html('<span style="color: red">Oops! Value must be a number</span>')
-		$('#linesDiv').addClass('errorInput')
-	}
-}
-
-function ignoreError(){
-		$('#error').html('');
-		$('#linesDiv').removeClass('errorInput')
-}
-
-
-//Uncomment for spinner - functional but does not match drop down
-// $('#lines').spinner({ min: 0, max: 1000 }).spinner('value',25);
-*/
 </script>
