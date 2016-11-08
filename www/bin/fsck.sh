423cf9e5c2ba96c888d6e3e28e115aad09b59f5c
Kateryna Pietukhova <kateryna.pietukhova@sirisoftware.com>
2016-11-08 18:48:21 +0200

    Fixes after testing to #296

--- www/bin/fsck.sh
+++ www/bin/fsck.sh
@@ -5,7 +5,7 @@ logger "Filesystem check has been started."
 while true; do
 	ERR=$(dmesg | grep "Remounting filesystem read-only")
 	if [ -z "$ERR" ]; then
-		sleep 90
+		sleep 15
 	else
 		logger "Read-only ERROR was detected."
 		fsck.ext4 -y /dev/$DEVICE
