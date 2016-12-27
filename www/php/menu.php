77d5c3e550b5e643f35e79874328621c9b10efe9
William Haynes <william@sabaitechnology.com>
2016-12-27 17:04:30 -0500

    Update menu.php
    Added OpenVPN Server

--- www/php/menu.php
+++ www/php/menu.php
@@ -21,8 +21,9 @@
 	 <li>
 	<a id='menu_vpn' class='superMenuLink button'>VPN</a>
 	<ul id='submenu_vpn' class='subMenu'>
-	 <li><a id='menu_vpn_pptpclient' href='?panel=vpn&section=pptpclient' class='subMenuLink button'>PPTP Client</a></li>
+	 <li><a id='menu_vpn_openvpnserver' href='?panel=vpn&section=openvpnserver' class='subMenuLink button'>Server</a></li>
 	 <li><a id='menu_vpn_openvpnclient' href='?panel=vpn&section=openvpnclient' class='subMenuLink button'>OpenVPN Client</a></li>
+	 <li><a id='menu_vpn_pptpclient' href='?panel=vpn&section=pptpclient' class='subMenuLink button'>PPTP Client</a></li>
 	 <li><a id='menu_vpn_tor' href='?panel=vpn&section=tor' class='subMenuLink button'>Tor</a></li>
 	 <li><a id='menu_vpn_gateways' href='?panel=vpn&section=gateways' class='subMenuLink button'>Gateways</a></li>
 	</ul>
